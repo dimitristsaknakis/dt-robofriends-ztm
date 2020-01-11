05a92ab55e37def5d327b77d78096315ec736316
Dimitris Tsaknakis <dimitristsaknakis@gmail.com>
2020-01-11 03:42:34 +0200

    Connected App to Redux state via connect(); mapStateToProps, mapDispatchToProps

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.module.css";
+
+// Import Redux actions
+import { setSearchField } from "../actions";
+
+// Tell App what state to listen to (the 'searchField' state from the
+// 'searchRobots' reducer)
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField // Temporarily bc only one reducer
+    }
+};
+
+// Tell App what Redux dispatch (action) it should listen to
+const mapDispatchToProps = dispatch => {
+    return {
+        // Accepts an event and returns the result of the dispatch of the
+        // 'setSearchField' action with the input text passed to it, to the reducer
+        onSearchChange: event => dispatch(setSearchField(event.target.value))
+    }
+};
+
 
 class App extends Component {
     constructor(props) {
@@ -11,31 +33,26 @@ class App extends Component {
         // Adding/creating state for App
         this.state = {
             robots: [],  // start out with empty array
-            searchfield: ""
         }
     };
 
     /* Lifecycle methods */
-    componentDidMount() {
+    componentDidMount() {        
         // Get users from online API; set 'robots' state with them
         fetch("https://jsonplaceholder.typicode.com/users")
         .then(response => response.json()) // convert response obj to JSON
         .then(users => this.setState({ robots: users })); // set 'robots' state to result
     };
 
-    // Event handler function for input onChange event (passed to SearchBox)
-    // It now has an arrow syntax so that 'this' belongs to App (not to input)
-    onSearchChange = (event) => {
-        // Update the state (setState must be used) before filtering
-        this.setState({ searchfield: event.target.value });
-    };
-
+    
     render() {
-        const { robots, searchfield } = this.state; // destructure from state
+        const { robots } = this.state; // destructure from local state
+        // Destructure from Redux state props
+        const { searchField, onSearchChange } = this.props;
 
         // Filter robots array, return name of the one typed in searchfield
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
 
         // If fetch() hasn't completed/resolved yet, show 'loading' msg
@@ -43,9 +60,9 @@ class App extends Component {
             <h1 className="tc">Loading...</h1> :
         (
             <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
+                <h1 className="f1">dt-RoboFriends-ztm</h1>
                 {/* Pass event handler function to SearchBox */}
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundary>
                         {/* Add CardList; it gets passed a 'robots' prop */}
@@ -57,4 +74,5 @@ class App extends Component {
     }
 };
 
-export default App; // default for one item; avoids destructuring
+// Use Redux naming convention for the connect() HOC parameters
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "tachyons";
 import './index.css';
 import App from "./containers/App"; // parent of all components
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a Redux store for the app.
+const store = createStore(searchRobots); // for now just pass this reducer
+
+ReactDOM.render(
+    // Wrap Provider around App, pass Provider the Redux store
+    // so that it's available to all components
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

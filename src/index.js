bf735c8d593e77de5129ccf5df4261d9a110f023
Dimitris Tsaknakis <dimitristsaknakis@gmail.com>
2020-01-11 04:12:31 +0200

    Installed and added the 'redux-logger' middleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
 import "tachyons";
 import './index.css';
 import App from "./containers/App"; // parent of all components
 import * as serviceWorker from './serviceWorker';
+
+// Import reducers
 import { searchRobots } from "./reducers";
 
-// Create a Redux store for the app.
-const store = createStore(searchRobots); // for now just pass this reducer
+// Create a 'redux-logger' middleware instance
+const logger = createLogger();
+
+// Create a Redux store for the app. Also apply the 'redux-logger' middleware
+const store = createStore(searchRobots, applyMiddleware(logger)); // for now just pass this reducer
 
 ReactDOM.render(
     // Wrap Provider around App, pass Provider the Redux store
